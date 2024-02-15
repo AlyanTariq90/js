ef67623ff0c2398c9f811580f55b1aa4373c4fd4
AlyanTariq90 <156662371+AlyanTariq90@users.noreply.github.com>
2024-02-15 22:10:30 +0500

    Add files via upload

---
+++ expertizo alyan.js/alyan.js
@@ -0,0 +1,111 @@
+// chapter 1  Question 1 
+
+// var greet = "Hello! Visitor how are you doing?"
+// alert(greet)
+
+
+// chapter 1 Question 2
+
+// var error = "Error! Please enter a valid password"
+// alert(error)
+
+
+// chapter 1 Question 3
+
+// var welcome = "Welcome to JS land... \nHappy Coding"
+// alert(welcome)
+
+
+// chapter 1 Question 4
+
+// var welcome = "Welcome to JS land..."
+// alert(welcome)
+// var code = "Happy Coding!"
+// alert(code)
+
+
+// chapter 1 Question 5 will run in browser's console
+
+
+
+
+// chapter 2 Question 1
+
+// var username;
+
+
+// chapter 2 Question 2
+
+// var myName = "Basil khan"
+
+
+// chapter 2 Question 3
+
+// var message = " Hello World"
+// alert(message)
+
+
+// chapter 2 Question 4 
+
+// var fullName = "John Doe"
+// var age = "15"
+// var profession = "Certified Mobile App Development"
+// alert(fullName)
+// alert(age)
+// alert(profession)
+
+
+// chapter 2 Question 5
+
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(pizza)
+
+
+// chapter 2 Question 6
+
+// var email = "basilkhan1250@gmail.com"
+// alert("My email address is " + email)
+
+
+// chapter 2 Question 7
+
+// var book = "A Smarter Way To Learn Javascript"
+// alert("I'm trying to learn form the book " + book)
+
+
+// chapter 2 Question 8
+
+// var script = "Yah! I can write HTML content through Javascript"
+// document.write(script)
+
+
+// chapter 2 Question 9
+
+// var symbol = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(symbol)
+
+// chapter 3 Question 1
+
+// var age = 19
+// alert("I'm " + age + " years old")
+
+
+// chapter 3 Question 2
+
+// var visits = "You have visited this site 14 times"
+// alert(visits)
+
+
+// chapter 3 Question 3
+
+// var birthYear = 2004
+// document.write("My birth year is " + birthYear) 
+// document.write( "<br/>" + "Data type of my declared variable is " + typeof(birthYear))
+
+
+// chapter 3 Question 4
+
+// var user = prompt("enter you name")
+// var product = prompt("how many shirts you want")
+// var store = "XYZ-Clothingstore"
+// document.write(user + " ordered " + product + " on " + store )
